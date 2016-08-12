e8b593e92f96339f95a7eca48aeecf0b72cdf3e4
vsk516 <vsk516@gmail.com>
2016-08-11 23:45:12 -0400

    Initial commit

---
+++ app/src/main/java/com/example/vamsisaikrishna/boardcastreceiver/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.vamsisaikrishna.boardcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private IntentFilter filter = new IntentFilter(Intent.ACTION_TIME_TICK);
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(getBaseContext(), "Action_Time_Tick intent Received", Toast.LENGTH_LONG).show();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onResume() {
+        this.registerReceiver(receiver, filter);
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onPause() {
+        this.unregisterReceiver(receiver);
+        super.onPause();
+    }
+}
